6349716a2d9dc4eaa90c7110de17b576195c3124
jonny <jonny@pinae.net>
2013-11-23 20:44:05 +0100

    added docstrings

--- librarian/views.py
+++ librarian/views.py
@@ -1,9 +1,24 @@
+# coding=utf-8
+"""
+This File is part of Pinyto
+"""
+
 from api_prototype.views import PinytoAPI
 from service.response import *
 
 
 class Librarian(PinytoAPI):
+    """
+    This class is used for managing books.
+    """
+
     def view(self, request):
+        """
+        Public View
+
+        @param request:
+        @return: json
+        """
         request_type = request.GET.get('type', 'index')
         if request_type == 'index':
             books = self.find({'type': 'book'})
