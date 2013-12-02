8623053251081217e1cb449bfd870eb2fd444261
jonny <jonny@pinae.net>
2013-12-02 19:22:11 +0100

    docstrings

--- service/database.py
+++ service/database.py
@@ -1,6 +1,17 @@
+# coding=utf-8
+"""
+This File is part of Pinyto
+"""
 from datetime import datetime
 
+
 def remove_underscore_fields(data):
+    """
+    Removes _id
+
+    @param data: dict
+    @return: dict
+    """
     converted = {}
     for key in data:
         if key[0] != '_':
@@ -12,6 +23,12 @@ def remove_underscore_fields(data):
 
 
 def remove_underscore_fields_list(data_list):
+    """
+    Removes _id for every dict in the list
+
+    @param data_list: [dict]
+    @return: [dict]
+    """
     converted_list = []
     for item in data_list:
         converted_list.append(remove_underscore_fields(item))
