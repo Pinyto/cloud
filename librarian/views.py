728a957c92691beff73b74b188b5ce7e00d54735
jonny <jonny@pinae.net>
2013-12-02 20:43:22 +0100

    search

--- librarian/views.py
+++ librarian/views.py
@@ -23,8 +23,24 @@ class Librarian(PinytoAPI):
         request_type = request.GET.get('type')
         print(request_type)
         if request_type == 'index':
-            books = self.find({'type': 'book'})
+            ean = request.GET.get('ean')
+            isbn = request.GET.get('isbn')
+            if ean:
+                books = self.find({'type': 'book', 'ean': ean})
+            elif isbn:
+                books = self.find({'type': 'book', 'isbn': isbn})
+            else:
+                books = self.find({'type': 'book'})
             print(books)
+            return json_response({'index': books})
+        elif request_type == 'search':
+            search_string = request.GET.get('searchstring')
+            print('Serching for: ' + search_string)
+            books = self.find({'type': 'book',
+                               '$or': [
+                                   {'title': {'$regex': search_string, '$options': 'i'}},
+                                   {'description': {'$regex': search_string, '$options': 'i'}}
+                               ]})
             return json_response({'index': books})
         else:
             print("wrong Type")
