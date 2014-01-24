373f1218eced261c83889465da5d64b390fc1b51
pinae <jonny@pinae.net>
2014-01-25 00:34:44 +0100

    Using a separate collection for each user.

--- database/views.py
+++ database/views.py
@@ -4,9 +4,10 @@ This File is part of Pinyto
 """
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login as backend_login
 from project_path import project_path
 from pymongo import MongoClient
+from pymongo.collection import Collection
 from service.response import json_response
 from datetime import datetime
 import json
@@ -34,6 +35,7 @@ def login(request):
     if user is not None:
         # the password verified for the user
         if user.is_active:
+            backend_login(request, user)
             return json_response({'authenticated': True})
         else:
             return json_response({'authenticated': False,
@@ -52,16 +54,19 @@ def store(request):
 
     :param request:
     """
-    data = request.POST.get('data')
-    data_type = request.POST.get('type')
-    if data and data_type:
-        db = MongoClient().pinyto.data
-        document = {'type': data_type,
-                    'time': datetime.utcnow(),
-                    'data': json.loads(data)}
-        db.insert(document)
-        return json_response({'success': True})
+    if request.user.is_authenticated():
+        data = request.POST.get('data')
+        data_type = request.POST.get('type')
+        if data and data_type:
+            db = Collection(MongoClient().pinyto, request.user.username)
+            document = {'type': data_type,
+                        'time': datetime.utcnow(),
+                        'data': json.loads(data)}
+            db.insert(document)
+            return json_response({'success': True})
+        else:
+            return json_response({'error': "If you want to store data you have to send your " +
+                                           "data as json string in a POST request in the parameter 'data'. " +
+                                           "You also have to supply a type string for the data."})
     else:
-        return json_response({'error': "If you want to store data you have to send your " +
-                                       "data as json string in a POST request in the parameter 'data'. " +
-                                       "You also have to supply a type string for the data."})
+        return json_response({'error': "You have to log in before you are allowed to store data."})
