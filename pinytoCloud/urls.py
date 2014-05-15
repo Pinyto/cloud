398612ef6b23f2565cb172f23ed6ac34488d454b
jonny <jonny@pinae.net>
2014-05-16 03:14:01 +0900

    Cleaned up urls

--- pinytoCloud/urls.py
+++ pinytoCloud/urls.py
@@ -19,16 +19,6 @@ urlpatterns += patterns(
     'database.views',
 
     url(r'^store$', 'store', name='store'),
-    #url(r'^\.login$', 'login', name='login'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-
-    #url(r'^[^.].*', 'home', name='home'),
-    #url(r'^$', 'home', name='home'),
 )
 
 urlpatterns += patterns(
