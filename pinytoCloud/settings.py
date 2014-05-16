2da88239d91a447f90de87ec087d162a1fb97830
jonny <jonny@pinae.net>
2014-05-16 17:50:54 +0900

    Missing comma

--- pinytoCloud/settings.py
+++ pinytoCloud/settings.py
@@ -134,7 +134,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'corsheaders'
+    'corsheaders',
     'pinytoCloud',
     'database',
     'api_prototype',
