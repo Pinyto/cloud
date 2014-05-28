ccd2c1f1bf187bbbe812d5cf3318778b18308443
jonny <jonny@pinae.net>
2014-05-28 17:27:15 +0900

    Debug assertion

--- pinytoCloud/views.py
+++ pinytoCloud/views.py
@@ -32,6 +32,7 @@ def authenticate(request):
     except IndexError:
         return json_response({'error': "This is not a registered public key of this user."})
     user.start_session(key)
+    assert user.sessions.filter(key=key).all()[0].token == 'TOKEN'
     encrypted_token = user.sessions.filter(key=key).all()[0].get_encrypted_token()
     hasher = sha256()
     hasher.update(encrypted_token)
