12e370546946b6e9c0a5560d2953135d4672fc7d
jonny <jonny@pinae.net>
2014-07-04 12:38:18 +0900

    added test suite

--- service/tests/__init__.py
+++ service/tests/__init__.py
@@ -1,7 +1,10 @@
 # coding=utf-8
+"""
+This File is part of Pinyto
+"""
 import unittest
 
-from service.tests import test_database, test_response, test_parsehtml
+from service.tests import test_database, test_response, test_parsehtml, test_http
 
 
 def suite():
@@ -14,6 +17,7 @@ def suite():
     test_suites = [
         tests_loader(test_database),
         tests_loader(test_response),
-        tests_loader(test_parsehtml)
+        tests_loader(test_parsehtml),
+        tests_loader(test_http)
     ]
     return unittest.TestSuite(test_suites)
