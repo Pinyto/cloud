d45bdd8feb8ff65baa3910b4e42d62d815f1162d
jonny <jonny@pinae.net>
2014-07-07 00:45:44 +0900

    Checking the token on every API call.

--- api/tests/test_bborsalino.py
+++ api/tests/test_bborsalino.py
@@ -4,11 +4,12 @@ This File is part of Pinyto
 """
 
 from django.test import TestCase
+from mock import patch
 from django.test.client import Client
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from service.database import CollectionWrapper
-from pinytoCloud.models import User
+from pinytoCloud.models import User, StoredPublicKey
 import json
 
 
@@ -16,36 +17,48 @@ class TestBBorsalino(TestCase):
     def setUp(self):
         self.bborsalino = User(name='bborsalino')
         self.bborsalino.save()
-        self.collection = Collection(MongoClient().pinyto, 'bborsalino')
-        backup_collection = Collection(MongoClient().pinyto, 'bborsalino_backup')
+        self.collection = Collection(MongoClient().pinyto, 'Hugo')
+        backup_collection = Collection(MongoClient().pinyto, 'Hugo_backup')
         for doc in self.collection.find():
             backup_collection.insert(doc)
         self.collection.drop()
-        self.collection = Collection(MongoClient().pinyto, 'bborsalino')
+        self.collection = Collection(MongoClient().pinyto, 'Hugo')
         self.collection_wrapper = CollectionWrapper(self.collection)
 
     def tearDown(self):
         self.collection.drop()
-        self.collection = Collection(MongoClient().pinyto, 'bborsalino')
-        backup_collection = Collection(MongoClient().pinyto, 'bborsalino_backup')
+        self.collection = Collection(MongoClient().pinyto, 'Hugo')
+        backup_collection = Collection(MongoClient().pinyto, 'Hugo_backup')
         for doc in backup_collection.find():
             self.collection.insert(doc)
 
+    def mock_check_token(self):
+        """
+        Mocks the token check.
+        @return: Session
+        """
+        hugo = User(name='Hugo')
+        hugo.save()
+        key = StoredPublicKey.create(hugo, '13', 5)
+        return hugo.start_session(key)
+
+    @patch('pinytoCloud.checktoken.check_token', mock_check_token)
     def test_index(self):
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-943176-24-7"}})
         test_client = Client()
-        response = test_client.post('/bborsalino/Librarian/index', {'ean': '1234567890123'})
+        response = test_client.post('/bborsalino/Librarian/index', {'token': 'fake', 'ean': '1234567890123'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content)['index'], [])
-        response = test_client.post('/bborsalino/Librarian/index', {'isbn': '978-3-943176-24-7'})
+        response = test_client.post('/bborsalino/Librarian/index', {'token': 'fake', 'isbn': '978-3-943176-24-7'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(json.loads(response.content)['index']), 1)
         self.assertEqual(json.loads(response.content)['index'][0]['data']['isbn'], u'978-3-943176-24-7')
-        response = test_client.post('/bborsalino/Librarian/index', {})
+        response = test_client.post('/bborsalino/Librarian/index', {'token': 'fake'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(json.loads(response.content)['index']), 1)
         self.assertEqual(json.loads(response.content)['index'][0]['data']['isbn'], u'978-3-943176-24-7')
 
+    @patch('pinytoCloud.checktoken.check_token', mock_check_token)
     def test_search(self):
         self.collection.insert({"type": "book",
                                 "data": {"isbn": "978-3-943176-24-7", "author": "Fels, Kerstin ; Fels, Andreas",
@@ -72,29 +85,31 @@ class TestBBorsalino(TestCase):
                                          "languages": "Deutsch (ger)", "edition": "1. Aufl.", "ean": "9783827373373",
                                          "place": "Arbeitszimmer", "year": 2009}})
         test_client = Client()
-        response = test_client.post('/bborsalino/Librarian/search', {'searchstring': 'Informatik'})
+        response = test_client.post('/bborsalino/Librarian/search', {'token': 'fake', 'searchstring': 'Informatik'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(json.loads(response.content)['index']), 2)
         self.assertEqual(json.loads(response.content)['index'][0]['data']['isbn'], u'978-3-8085-3004-7')
         self.assertEqual(json.loads(response.content)['index'][1]['data']['isbn'], u'978-3-8273-7337-3')
 
+    @patch('pinytoCloud.checktoken.check_token', mock_check_token)
     def test_update(self):
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-943176-24-7", "place": "A"}})
-        book = self.collection.find()[0]
+        book = self.collection.find_one()
         book['data']['place'] = "B"
         book['_id'] = str(book['_id'])
         test_client = Client()
-        response = test_client.post('/bborsalino/Librarian/update', {'book': json.dumps(book)})
+        response = test_client.post('/bborsalino/Librarian/update', {'token': 'fake', 'book': json.dumps(book)})
         self.assertEqual(response.status_code, 200)
         self.assertTrue(json.loads(response.content)['success'])
         book_test = self.collection.find()[0]
         self.assertEqual(book_test['data']['place'], u'B')
 
+    @patch('pinytoCloud.checktoken.check_token', mock_check_token)
     def test_update_all(self):
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-943176-24-7", "place": "A"}})
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-943176-24-7", "place": "B"}})
         test_client = Client()
-        response = test_client.post('/bborsalino/Librarian/update_all', {'book': json.dumps({
+        response = test_client.post('/bborsalino/Librarian/update_all', {'token': 'fake', 'book': json.dumps({
             "type": "book", "data": {"isbn": "978-3-943176-24-7", "place": "C"}
         })})
         self.assertEqual(response.status_code, 200)
@@ -102,14 +117,15 @@ class TestBBorsalino(TestCase):
         for book in self.collection.find():
             self.assertEqual(book['data']['place'], u'C')
 
+    @patch('pinytoCloud.checktoken.check_token', mock_check_token)
     def test_duplicate(self):
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-943176-24-7", "author": "Max Mustermann"}})
-        book = self.collection.find()[0]
+        book = self.collection.find_one()
         del book['data']['author']
         self.assertFalse('author' in book['data'])
         book['_id'] = str(book['_id'])
         test_client = Client()
-        response = test_client.post('/bborsalino/Librarian/duplicate', {'book': json.dumps(book)})
+        response = test_client.post('/bborsalino/Librarian/duplicate', {'token': 'fake', 'book': json.dumps(book)})
         self.assertEqual(response.status_code, 200)
         self.assertTrue(json.loads(response.content)['success'])
         self.assertEqual(self.collection.find(
@@ -119,24 +135,26 @@ class TestBBorsalino(TestCase):
             self.assertEqual(book['data']['isbn'], u'978-3-943176-24-7')
             self.assertEqual(book['data']['author'], u'Max Mustermann')
 
+    @patch('pinytoCloud.checktoken.check_token', mock_check_token)
     def test_remove(self):
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-943176-24-7", "author": "Max Mustermann"}})
         self.assertEqual(self.collection.find({'type': "book"}).count(), 1)
         book = self.collection.find_one()
         book['_id'] = str(book['_id'])
         test_client = Client()
-        response = test_client.post('/bborsalino/Librarian/remove', {'book': json.dumps(book)})
+        response = test_client.post('/bborsalino/Librarian/remove', {'token': 'fake', 'book': json.dumps(book)})
         self.assertEqual(response.status_code, 200)
         self.assertTrue(json.loads(response.content)['success'])
         self.assertEqual(self.collection.find({'type': "book"}).count(), 0)
 
+    @patch('pinytoCloud.checktoken.check_token', mock_check_token)
     def test_statistics(self):
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-943176-24-7", "place": "A"}})
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-943176-24-7", "place": "B", "lent": "Hugo"}})
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-8085-3004-7", "place": "C"}})
         self.collection.insert({"type": "book", "data": {"isbn": "978-3-8273-7337-3", "place": "B"}})
         test_client = Client()
-        response = test_client.post('/bborsalino/Librarian/statistics')
+        response = test_client.post('/bborsalino/Librarian/statistics', {'token': 'fake'})
         self.assertEqual(response.status_code, 200)
         res = json.loads(response.content)
         self.assertEqual(res['book_count'], 4)
--- api_prototype/views.py
+++ api_prototype/views.py
@@ -28,8 +28,15 @@ def api_call(request, user_name, assembly_name, function_name):
     @param request: Django Request object
     @return: json response
     """
+    token = request.POST.get('token')
+    if not token:
+        return json_response({'error': "Unauthenticated API-calls are not supported. Please supply a token."})
+    session = check_token(token)
+    if not isinstance(session, Session):
+        # session is not a session so it has to be response object with an error message
+        return session
     try:
-        user = User.objects.filter(name=user_name).all()[0]
+        assembly_user = User.objects.filter(name=user_name).all()[0]
     except IndexError:
         return json_response(
             {'error': "The user " + user_name + " was not found. There can not be an assembly " +
@@ -41,10 +48,10 @@ def api_call(request, user_name, assembly_name, function_name):
     except ImportError:
         # There is no statically defined api function for this call. Proceed to
         # loading the code from the database and executing it in the sandbox.
-        return load_api(user, assembly_name, function_name)
+        return load_api(session, assembly_user, assembly_name, function_name)
     for name, function in getmembers(api_class, predicate=isfunction):
         if not unicode(name).startswith(u'job_') and unicode(name) == unicode(function_name):
-            collection = Collection(MongoClient().pinyto, user.name)  # TODO: this is not user_name but the authenticated user from session.user!
+            collection = Collection(MongoClient().pinyto, session.user.name)
             collection_wrapper = CollectionWrapper(collection)
             return HttpResponse(
                 function(
@@ -54,21 +61,22 @@ def api_call(request, user_name, assembly_name, function_name):
                 content_type='application/json')
     # If we reach this point the api_class was found but the function was not defined in the class.
     # So we try to load this code from the database.
-    return load_api(user, assembly_name, function_name)
+    return load_api(session, assembly_user, assembly_name, function_name)
 
 
-def load_api(user, assembly_name, function_name):
+def load_api(session, assembly_user, assembly_name, function_name):
     """
     There is no statically defined api function for this call. Proceed to
     loading the code from the database and executing it in the sandbox.
 
-    @param user: User
+    @param session: Session
+    @param assembly_user: User
     @param assembly_name: string
     @param function_name: string
     @return: Response object
     """
     try:
-        assemblies = user.assemblies.filter(name=assembly_name).all()
+        assemblies = assembly_user.assemblies.filter(name=assembly_name).all()
         if len(assemblies) > 1:
             return json_response(
                 {'error': "The user has more than one assembly of this name. That does not make any sense."}
@@ -76,17 +84,17 @@ def load_api(user, assembly_name, function_name):
         assembly = assemblies[0]
     except IndexError:
         return json_response(
-            {'error': "Assembly not found. Does " + user.name + " have an Assembly named " + assembly_name + "?"}
+            {'error': "Assembly not found. Does " + assembly_user.name + " have an Assembly named " + assembly_name + "?"}
         )
     try:
         api_function = assembly.api_functions.filter(name=function_name).all()[0]
     except IndexError:
         return json_response(
-            {'error': "The assembly " + user.name + "/" + assembly_name + ' exists but has no API function "' +
+            {'error': "The assembly " + assembly_user.name + "/" + assembly_name + ' exists but has no API function "' +
                       function_name + '".'}
         )
     print(api_function.code)
-    collection = Collection(MongoClient().pinyto, user.name)  # TODO: this is not user_name but the authenticated user from session.user!
+    collection = Collection(MongoClient().pinyto, session.user.name)
     collection_wrapper = CollectionWrapper(collection)
     response_data, time = safely_exec(api_function.code, collection_wrapper)  # TODO: pass response object
     print(time)
