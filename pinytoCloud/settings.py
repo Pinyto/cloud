1ea6f90177992a7e28174102fcc8a07172e93a15
jonny <jonny@pinae.net>
2014-09-18 03:31:35 +0900

    Removed unused imports

--- pinytoCloud/settings.py
+++ pinytoCloud/settings.py
@@ -125,9 +125,9 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
     #'django.contrib.auth',
-    'django.contrib.contenttypes',
+    #'django.contrib.contenttypes',
     #'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     #'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
