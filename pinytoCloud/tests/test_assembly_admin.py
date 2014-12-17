1174f7534dd18682b0a6e787edd977210aae8952
jonny <jonny@pinae.net>
2014-12-18 02:24:16 +0900

    Tests for get_assembly_source

--- pinytoCloud/tests/test_assembly_admin.py
+++ pinytoCloud/tests/test_assembly_admin.py
@@ -887,3 +887,143 @@ class TestUninstallAssembly(TestCase):
         self.assertIn('success', res)
         self.assertTrue(res['success'])
         self.assertEqual(self.hugo.installed_assemblies.count(), 0)
+
+
+class TestGetAssemblySource(TestCase):
+    def setUp(self):
+        self.hugo = User(name='Hugo')
+        self.hugo.save()
+        n = "4906219502681250223798809774327327904260276391419666181914677115202847435445452518005507304428444" + \
+            "4742603016009120644035348330282759333360784030498937872562985999515117742892991032749465423946790" + \
+            "9158556402591029134146090349452893554696956539933811963368734446853075386625683127394662795881747" + \
+            "0894364256604860146232603404588092435482734374812471361593625543917217088490113148478038251561381" + \
+            "8672330898366008493547872891602227800340728106862543870889614647176014952843513826946064902193374" + \
+            "5442573561655969372352609568579364393649500357127004050087969117303304927939643892730600997930439" + \
+            "1147195261326440509804663056089132784514383110912100463888066750648282272016554512713401644905102" + \
+            "0092897659089644083580577942453555759938724084685541443702341305828164318826796951735041984241803" + \
+            "8137353327025799036181291470746401739276004770882613670169229258999662110622086326024782780442603" + \
+            "0939464832253228468472307931284129162453821959698949"
+        key = StoredPublicKey.create(self.hugo, unicode(n), long(65537))
+        self.session = self.hugo.start_session(key)
+        self.hugo.last_calculation_time = timezone.now()
+        self.hugo.save()
+        pinyto_cipher = PKCS1_OAEP.new(PINYTO_PUBLICKEY)
+        self.authentication_token = b16encode(pinyto_cipher.encrypt(self.session.token))
+
+    def test_no_JSON(self):
+        response = self.client.post(
+            reverse('get_assembly_source'),
+            "Didelidi",
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        res = json.loads(response.content)
+        self.assertIn('error', res)
+        self.assertEqual(res['error'], "Please supply the data as JSON.")
+
+    def test_no_token(self):
+        response = self.client.post(
+            reverse('get_assembly_source'),
+            json.dumps({'x': 1234}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        res = json.loads(response.content)
+        self.assertIn('error', res)
+        self.assertEqual(res['error'], "Please supply JSON with a token key.")
+
+    def test_no_author(self):
+        response = self.client.post(
+            reverse('get_assembly_source'),
+            json.dumps({'token': self.authentication_token}),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        res = json.loads(response.content)
+        self.assertIn('error', res)
+        self.assertEqual(res['error'], "You have to supply an author to identify the assembly.")
+
+    def test_wrong_author(self):
+        response = self.client.post(
+            reverse('get_assembly_source'),
+            json.dumps({
+                'token': self.authentication_token,
+                'author': 'Bilbo'
+            }),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        res = json.loads(response.content)
+        self.assertIn('error', res)
+        self.assertEqual(res['error'], "There was no user found with the name Bilbo.")
+
+    def test_no_assembly(self):
+        response = self.client.post(
+            reverse('get_assembly_source'),
+            json.dumps({
+                'token': self.authentication_token,
+                'author': 'Hugo'
+            }),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        res = json.loads(response.content)
+        self.assertIn('error', res)
+        self.assertEqual(res['error'], "You have to supply the name of the assembly.")
+
+    def test_wrong_assembly(self):
+        response = self.client.post(
+            reverse('get_assembly_source'),
+            json.dumps({
+                'token': self.authentication_token,
+                'author': 'Hugo',
+                'name': 'Foo'
+            }),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        res = json.loads(response.content)
+        self.assertIn('error', res)
+        self.assertEqual(res['error'], "There was no assembly found with the name Hugo/Foo.")
+
+    def test_successful(self):
+        foo = Assembly(name='Foo', description='Bar', author=self.hugo)
+        foo.save()
+        func1 = ApiFunction(
+            name='func1',
+            code='print("Hallo Welt!")',
+            assembly=foo
+        )
+        func1.save()
+        func2 = ApiFunction(
+            name='func2',
+            code='print("Hello World!")',
+            assembly=foo
+        )
+        func2.save()
+        job1 = Job(
+            name='job1',
+            code='print("Arbeit.")',
+            assembly=foo,
+            schedule=0
+        )
+        job1.save()
+        response = self.client.post(
+            reverse('get_assembly_source'),
+            json.dumps({
+                'token': self.authentication_token,
+                'author': 'Hugo',
+                'name': 'Foo'
+            }),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        res = json.loads(response.content)
+        self.assertNotIn('error', res)
+        self.assertIn('api_functions', res)
+        self.assertListEqual(res['api_functions'], [{
+            u'name': u'func1',
+            u'code': u'print("Hallo Welt!")'
+        }, {
+            u'name': u'func2',
+            u'code': u'print("Hello World!")'
+        }])
+        self.assertIn('jobs', res)
+        self.assertListEqual(res['jobs'], [{
+            u'name': u'job1',
+            u'code': u'print("Arbeit.")',
+            u'schedule': 0
+        }])
