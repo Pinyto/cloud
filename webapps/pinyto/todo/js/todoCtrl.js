b2e0df284c7a0aded7c990d5f54cab355e0e047f
jonny <jonny@pinae.net>
2014-12-29 07:18:34 +0900

    Basic list

--- webapps/pinyto/todo/js/todoCtrl.js
+++ webapps/pinyto/todo/js/todoCtrl.js
@@ -2,11 +2,35 @@
 
 todoApp.controller('todoCtrl',
     function ($scope, Backend, Authenticate, $timeout) {
-    // Function Definitions
-    $scope.hasID = function (document) {
-        return !!document['_id'];
-    };
+        // Function Definitions
+        $scope.hasID = function (document) {
+            return !!document['_id'];
+        };
 
-    // Initialization
+        $scope.addTodo = function () {
+            $scope.unfinishedTodo.splice(0, 0, {'_id': undefined, text: ''});
+        };
 
-});
+        $scope.moveToFinished = function (index) {
+            $scope.finishedTodo.splice(0, 0, $scope.unfinishedTodo[index]);
+            $scope.unfinishedTodo.splice(index, 1);
+        };
+
+        $scope.moveToUnfinished = function (index) {
+            $scope.unfinishedTodo.splice(0, 0, $scope.finishedTodo[index]);
+            $scope.finishedTodo.splice(index, 1);
+        };
+
+        $scope.deleteUnfinishedTodo = function (index) {
+            $scope.unfinishedTodo.splice(index, 1);
+        };
+
+        $scope.deleteFinishedTodo = function (index) {
+            $scope.finishedTodo.splice(index, 1);
+        };
+
+        // Initialization
+        $scope.unfinishedTodo = [];
+        $scope.finishedTodo = [];
+    }
+);
