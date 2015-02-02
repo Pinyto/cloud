7517c3466cc059e7672df237f95cce6c20285d1e
jonny <jonny@pinae.net>
2015-02-03 02:00:57 +0900

    Fixed json response test.

--- service/tests/test_response.py
+++ service/tests/test_response.py
@@ -4,6 +4,7 @@ from __future__ import division, print_function, unicode_literals
 from django.test import TestCase
 from django.http.response import HttpResponse
 from service.response import json_response
+import json
 
 
 class TestResponse(TestCase):
@@ -11,11 +12,19 @@ class TestResponse(TestCase):
         pass
 
     def test_json_response(self):
-        data = {'a': 12, 'b': ['c', 4, {1: 5, 2: 'd'}], 9: 14.5}
+        data = {'a': 12, 'b': ['c', 4, {'1': 5, '2': 'd'}], '9': 14.5}
         response = json_response(data)
         self.assertEqual(type(response), HttpResponse)
         self.assertEqual(200, response.status_code)
-        self.assertEqual(
-            '{"9": 14.5, "a": 12, "b": ["c", 4, {"1": 5, "2": "d"}]}',
-            str(response.content, encoding='utf-8')
-        )
+        res = json.loads(str(response.content, encoding='utf-8'))
+        self.assertIn('a', res)
+        self.assertEqual(res['a'], 12)
+        self.assertIn('b', res)
+        self.assertEqual(res['b'][0], 'c')
+        self.assertEqual(res['b'][1], 4)
+        self.assertIn('1', res['b'][2])
+        self.assertEqual(res['b'][2]['1'], 5)
+        self.assertIn('2', res['b'][2])
+        self.assertEqual(res['b'][2]['2'], 'd')
+        self.assertIn('9', res)
+        self.assertAlmostEqual(res['9'], 14.5, places=10)
