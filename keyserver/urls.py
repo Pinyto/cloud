3c44bf467067f1cf3b80dabe2571987bbceb2690
pinae <jonny@pinae.net>
2016-08-05 13:05:52 +0200

    Changed urls to simple lists

--- keyserver/urls.py
+++ keyserver/urls.py
@@ -17,12 +17,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from keyserver.views import authenticate, register, change_password
 
-urlpatterns = patterns(
-    'keyserver.views',
-
-    url(r'^authenticate$', 'authenticate', name='keyserver_authenticate'),
-    url(r'^register$', 'register', name='keyserver_register'),
-    url(r'^change_password$', 'change_password', name='change_password'),
-)
+urlpatterns = [
+    url(r'^authenticate$', authenticate, name='keyserver_authenticate'),
+    url(r'^register$', register, name='keyserver_register'),
+    url(r'^change_password$', change_password, name='change_password'),
+]
--- pinytoCloud/urls.py
+++ pinytoCloud/urls.py
@@ -17,49 +17,45 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from pinytoCloud.views import authenticate_request, logout, list_keys, set_key_active, delete_key, register_new_key
+from pinytoCloud.views import register_request, list_own_assemblies, save_assembly, delete_assembly
+from pinytoCloud.views import list_installed_assemblies, list_all_assemblies, install_assembly, uninstall_assembly
+from pinytoCloud.views import get_assembly_source, home
+from database.views import statistics, store
+from api_prototype.views import api_call
 
-urlpatterns = patterns(
-    'pinytoCloud.views',
+urlpatterns = [
+    url(r'^authenticate$', authenticate_request, name='authenticate'),
+    url(r'^logout$', logout, name='logout'),
+    url(r'^list_keys$', list_keys, name='list_keys'),
+    url(r'^set_key_active$', set_key_active, name='set_key_active'),
+    url(r'^delete_key$', delete_key, name='delete_key'),
+    url(r'^register_new_key$', register_new_key, name='register_new_key'),
+    url(r'^register$', register_request, name='register'),
+    url(r'^list_own_assemblies$', list_own_assemblies, name='list_own_assemblies'),
+    url(r'^save_assembly$', save_assembly, name='save_assembly'),
+    url(r'^delete_assembly$', delete_assembly, name='delete_assembly'),
+    url(r'^list_installed_assemblies$', list_installed_assemblies, name='list_installed_assemblies'),
+    url(r'^list_all_assemblies$', list_all_assemblies, name='list_all_assemblies'),
+    url(r'^install_assembly$', install_assembly, name='install_assembly'),
+    url(r'^uninstall_assembly$', uninstall_assembly, name='uninstall_assembly'),
+    url(r'^get_assembly_source$', get_assembly_source, name='get_assembly_source'),
+]
 
-    url(r'^authenticate$', 'authenticate_request', name='authenticate'),
-    url(r'^logout$', 'logout', name='logout'),
-    url(r'^list_keys$', 'list_keys', name='list_keys'),
-    url(r'^set_key_active$', 'set_key_active', name='set_key_active'),
-    url(r'^delete_key$', 'delete_key', name='delete_key'),
-    url(r'^register_new_key$', 'register_new_key', name='register_new_key'),
-    url(r'^register$', 'register_request', name='register'),
-    url(r'^list_own_assemblies$', 'list_own_assemblies', name='list_own_assemblies'),
-    url(r'^save_assembly$', 'save_assembly', name='save_assembly'),
-    url(r'^delete_assembly$', 'delete_assembly', name='delete_assembly'),
-    url(r'^list_installed_assemblies$', 'list_installed_assemblies', name='list_installed_assemblies'),
-    url(r'^list_all_assemblies$', 'list_all_assemblies', name='list_all_assemblies'),
-    url(r'^install_assembly$', 'install_assembly', name='install_assembly'),
-    url(r'^uninstall_assembly$', 'uninstall_assembly', name='uninstall_assembly'),
-    url(r'^get_assembly_source$', 'get_assembly_source', name='get_assembly_source'),
-)
-
-urlpatterns += patterns(
-    '',
-
+urlpatterns += [
     url(r'^keyserver/', include('keyserver.urls')),
-)
+]
 
-urlpatterns += patterns(
-    'database.views',
+urlpatterns += [
+    url(r'^(?P<user_name>\w+)/(?P<assembly_name>\w+)/store$', store, name='store'),
+    url(r'^statistics$', statistics, name='statistics'),
+]
 
-    url(r'^(?P<user_name>\w+)/(?P<assembly_name>\w+)/store$', 'store', name='store'),
-    url(r'^statistics$', 'statistics', name='statistics'),
-)
+urlpatterns += [
+    url(r'^(?P<user_name>\w+)/(?P<assembly_name>\w+)/(?P<function_name>\w+)$', api_call, name='api_call'),
+]
 
-urlpatterns += patterns(
-    'api_prototype.views',
-
-    url(r'^(?P<user_name>\w+)/(?P<assembly_name>\w+)/(?P<function_name>\w+)$', 'api_call', name='api_call'),
-)
-
-urlpatterns += patterns(
-    'pinytoCloud.views',
-
-    url(r'^.*', 'home', name='home'),
-)
+urlpatterns += [
+    url(r'^.*', home, name='home'),
+]
