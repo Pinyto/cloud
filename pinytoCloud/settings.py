64c24734cc34ae540f558c059cdae71de9bcc1e0
Pina Merkert <pina@pinae.net>
2019-05-12 06:07:30 +0200

    Added directory for staticfiles

--- pinytoCloud/settings.py
+++ pinytoCloud/settings.py
@@ -81,7 +81,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = './media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -92,7 +92,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = './static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
